af068956e8f5539e9e37bdd2c45572510ed3b395
NayemHassan <nayem15-12498@diu.edu.bd>
2024-02-28 17:26:05 +0600

    nayem

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Backend\FooterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+//Custom Api
+Route::post('/doctor/api',['ApiController','DoctorApi']);
+//Custom Api End
 Route::prefix('api')->group(function () {
     // GET request to retrieve all footer data
     Route::get('/footer-data', [FooterController::class, 'getAllFooterData']);
